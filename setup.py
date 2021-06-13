44d3fb0c1372ac1fe7f75332c142859f38528833
Agung Yuliyanto <agung.96tm@gmail.com>
2021-06-13 20:57:28 +0700

    update setup.py, more clearly

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='notelink',
     version='0.0.1',
-    author='Agung Yuliyanto',
-    author_email='agung.96tm@gmail.com',
     description='Management your favorite link using CLI.',
     long_description='Management your favorite link using CLI.',
+    author='Agung Yuliyanto',
+    author_email='agung.96tm@gmail.com',
+    url='https://github.com/agung96tm/notelink',
+    project_urls={
+        'Source Code': 'https://github.com/agung96tm/notelink',
+    },
+    license_files=('LICENCE',),
     packages=find_packages(
-        exclude=['tests', 'docs',]
+        exclude=['tests', 'docs']
     ),
     include_package_data=True,
     install_requires=[i.strip() for i in open("requirements.txt").readlines()],
